09ec8d566d3d14d234ccbf412ca9a8b5e8e99f4b
Hack Reactor Student <student@hackreactor.com>
2017-06-23 12:31:28 -0500

    TSWIFT

---
+++ script.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+$("#1").click(function(){
+   window.location.href="buyticket.html";
+})
+
+$("#2").click(function(){
+   window.location.href="https://taylorswift.com/events";
+})
+
+$("#3").click(function(){
+   window.location.href="https://taylorswift.com/events";
+})
+
+$("#4").click(function(){
+   window.location.href="buyticket.html";
+})
+
+$("#buy").click(function(){
+window.location.href="http://store.taylorswift.com/Seagull-Long-Sleeved-Top.html"
+})
+
+$("#clothes").click(function(){
+window.location.href="http://store.taylorswift.com/1989-Merch.html"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
