e3248b999424d4ec7e997a4d8c0f9a3ba4018007
Hack Reactor Student <student@hackreactor.com>
2017-06-23 14:33:18 -0500

    Expanded

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+  /* Your Javascript Here */
   console.log("page is loaded");
+  $(".pictureBoxes").click(function(event){
+    $("#pictureDisplay").childr
+    var pictureClicked = event.target.src;
+    console.log(event.target.src);
+    var test = "#displayBox";
+    $(test).attr("src", pictureClicked);
+  });
 });
