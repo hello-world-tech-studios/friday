aab05e0fb8168a80624fc54510ca5a184289cb76
Vincent Nguyen <secretfalcoln@yahoo.com>
2017-06-24 07:21:21 -0500

    Changed buyButton so when href is empty, it will alert to select an item

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
 
   /* Your Javascript Here */
   console.log("page is loaded");
+    $("#buyButton").click(function(event){
+      console.log(event.target.href);
+      var hrefString = $("#buyButton").attr("href")
+      console.log($("#buyButton").attr("href"))
+      if (hrefString == "") {
+        console.log("string empty");
+        alert("You have not selected an item!");
+      } else {
+        console.log("string full");
+      }
+    });
   $(".pictureBoxes").click(function(event){
     $("#pictureDisplay").childr
     var pictureClicked = event.target.src;
